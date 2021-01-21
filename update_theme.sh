82de1581c0a0cac5c880c163f7f8b911b1759e18
Sky Ao <aoxiaojian@gmail.com>
2021-01-21 23:54:39 +0800

    add script to link node_modules

--- update_theme.sh
+++ update_theme.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+if [ -d "node_modules" ];then
+  echo 'Find directoy "node_modules"'
+else
+  echo 'Directoy "node_modules" not found, create soft link'
+  ln -s ../node_modules .
+fi
+
 if [ ! -d "themes" ];then
   echo "No themes directory, create it"
   mkdir themes
